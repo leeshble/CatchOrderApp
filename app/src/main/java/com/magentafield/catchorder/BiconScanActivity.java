8691105ffe225a0d6aececa6437a01330ef508b0
LeeSangHyun <leeshble@gmail.com>
2021-12-22 00:03:42 +0900

    [Project file upload]

---
+++ app/src/main/java/com/magentafield/catchorder/BiconScanActivity.java
@@ -0,0 +1,14 @@
+package com.magentafield.catchorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BiconScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bicon_scan);
+    }
+}
